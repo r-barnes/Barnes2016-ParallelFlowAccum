f7c4ad92de33ea3c3939b14386f1541627b65e98
Richard Barnes <rbarnes@umn.edu>
2016-07-23 12:44:18 -0600

    Have everything reference generic job

---
+++ tests/generic_job.sh
@@ -0,0 +1,17 @@
+if [ "$gCORES" -gt $((24*SLURM_JOB_NUM_NODES)) ]; then
+  module load boost/1.55.0
+  module load intel/2015.2.164
+  module load mvapich2_ib
+
+  gLAYOUT=/oasis/scratch/comet/rbarnes1/temp_project/gisdata/$gDATASET/$gLAYER/$gLAYOUTNAME
+  gOUTPUTDIR=/oasis/scratch/comet/rbarnes1/temp_project/gisdata/$gDATASET/$gOUTLAYER
+
+  rm -rf   $gTEMPDIR
+  mkdir -p $gTEMPDIR
+  rm -rf   $gOUTPUTDIR
+  mkdir -p $gOUTPUTDIR
+
+  $gMPI -n $gCORES $gEXE $gONEMANY $gEVICT $gLAYOUT $gOUTPUTDIR/%f.tif
+else
+  echo "Insufficient cores available!"
+fi
